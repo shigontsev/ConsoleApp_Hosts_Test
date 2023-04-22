a84269c23322911a78577a9b6d1520d3465cd286
shigontsev <shigontsev1@mail.ru>
2023-04-22 15:35:19 +0400

    refactor

--- ConsoleApp_Hosts_Test/Program.cs
+++ ConsoleApp_Hosts_Test/Program.cs
@@ -1,15 +1,12 @@
-﻿using ConsoleApp_Hosts_Test;
-using System;
-using System.Data.SqlTypes;
-using System.Text.RegularExpressions;
-
-internal class Program
+﻿namespace ConsoleApp_Hosts_Test
 {
-    private static void Main(string[] args)
+    internal class Program
     {
-        Console.WriteLine("Hello, World!");
-        string[] A = new[]
+        private static void Main(string[] args)
         {
+            Console.WriteLine("Hello, World!");
+            string[] A = new[]
+            {
             "unlock.microvirus.md",
             "visitwar.com",
             "visitwar.de",
@@ -47,15 +44,53 @@ internal class Program
             //"australia.open.com",
             //"credit.card.us"
         };
-        string[] B = new[]
-        {
+            string[] B = new[]
+            {
             "microvirus.md",
             "visitwar.de",
             "piratebay.co.uk",
             "list.stolen.credit.card.us",
         };
 
-        Console.WriteLine(String.Join("-", Solution.solution(A, B)));
-        //Console.WriteLine(String.Join("-", Solution.solution1(A, B)));
+            Console.WriteLine(string.Join("-", Solution.solution(A, B)));
+            //Console.WriteLine(String.Join("-", Solution.solution1(A, B)));
+        }
+    }
+
+    public class Solution
+    {
+        public static int[] solution(string[] A, string[] B)
+        {
+            var list = new HashSet<string>(A);
+            foreach (var itemB in B)
+            {
+                list.RemoveWhere(x => x.EndsWith(itemB));
+            }
+
+            return Enumerable.Range(0, A.Length).Where(i => list.Contains(A[i])).ToArray();
+        }
+
+        //public static int[] solution1(string[] A, string[] B)
+        //{
+        //    if (B.Length == 0)
+        //        return Enumerable.Range(0, A.Length).ToArray();
+        //    if (A.Length == 0)
+        //        return new int[0];
+
+        //    var list = new HashSet<string>(A);
+        //    foreach (var str in list)
+        //    {
+        //        foreach (var itemB in B)
+        //        {
+        //            if (str.EndsWith(itemB))
+        //            {
+        //                list.Remove(str);
+        //                break;
+        //            }
+        //        }
+        //    }
+
+        //    return Enumerable.Range(0, A.Length).Where(i => list.Contains(A[i])).ToArray();
+        //}
     }
 }
